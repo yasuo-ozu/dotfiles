e5db640d5a25abfbc5c00047c0741ddfecf3e172
yozu <yasuo@ozu.email>
2018-09-18 07:01:11 +0900

    Modified modules

--- platform-cookbooks/arch/home/default.rb
+++ platform-cookbooks/arch/home/default.rb
@@ -1,8 +1,18 @@
 
+directory "~/.config"
+[ "~/.gnupg", "~/.ssh"].each do |dir|
+	directory dir do
+		owner node[:user]
+		group node[:user]
+		mode "600"
+	end
+end
+
 package 'xdg-user-dirs'
 
 execute 'create directories in home' do
 	command 'LC_ALL=C xdg-user-dirs-update'
 	not_if '[ -e "~/.config/user-dirs.dirs" ]'
 end
+
 
