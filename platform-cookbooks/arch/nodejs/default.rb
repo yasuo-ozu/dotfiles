971333e55f38da8e4efa031c5245da3e90f52f30
yozu <yasuo@ozu.email>
2018-10-16 16:07:49 +0900

    add npm pkgs

--- platform-cookbooks/arch/nodejs/default.rb
+++ platform-cookbooks/arch/nodejs/default.rb
@@ -1,3 +1,13 @@
 
 package 'nodejs'
 package 'npm'
+
+execute "install gulp" do
+	not_if 'npm -g list gulp'
+	command 'npm -g install gulp'
+end
+
+execute "install nodemon" do
+	not_if 'npm -g list nodemon'
+	command 'npm -g install nodemon'
+end
